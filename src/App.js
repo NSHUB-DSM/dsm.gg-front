d5d83b4ad0bce2e42de1935d8e8141f1650ded9f
woosang <woosang3078@hanmail.net>
2022-12-17 23:20:41 +0900

    ⚡ :: 로그인 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import StyleProvider from './styles';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import MainPage from './pages/Main/main';
-import SignupPage from './pages/Signup/signup';
+import StyleProvider from "./styles";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import MainPage from "./pages/Main/main";
+import SignupPage from "./pages/Signup/signup";
+import LoginPage from "./pages/Login/login";
 
 export default function App() {
   return (
@@ -12,8 +13,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/user/signup" element={<SignupPage />}></Route>
+          <Route path="/user/login" element={<LoginPage />}></Route>
         </Routes>
       </BrowserRouter>
     </StyleProvider>
-  )
+  );
 }
