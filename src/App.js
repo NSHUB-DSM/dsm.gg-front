daa01571cd44fc7cceda78b1397e4d30b6619df4
woosang <woosang3078@hanmail.net>
2022-12-17 23:06:48 +0900

    ⚡ :: 메인 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import StyleProvider from './styles';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import MainPage from './pages/Main/main';
 
 export default function App() {
   return (
     <StyleProvider>
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<MainPage />}></Route>
         </Routes>
       </BrowserRouter>
     </StyleProvider>
