38cf925217dec2ad446cbad20c366fc3cbee092d
woosang <woosang3078@hanmail.net>
2022-12-19 14:52:29 +0900

    ⚡ :: 커뮤니티 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 import MainPage from "./pages/Main/main";
 import SignupPage from "./pages/Signup/signup";
 import LoginPage from "./pages/Login/login";
+import CommunityPage from './pages/Community/notice';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/user/signup" element={<SignupPage />}></Route>
           <Route path="/user/login" element={<LoginPage />}></Route>
+          <Route path="/notice" element={<CommunityPage />}></Route>
         </Routes>
       </BrowserRouter>
     </StyleProvider>
