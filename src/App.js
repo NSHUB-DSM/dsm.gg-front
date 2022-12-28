f5ea779e1ba99df2cd08c9d006ce7451d4878abc
woosang <woosang3078@hanmail.net>
2022-12-28 13:03:42 +0900

    ⚡ :: 커뮤니티 조회 페이지 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MatchingPage from './pages/Matching/matching';
 import WritePage from './pages/Community/write';
 import RankingPage from "./components/ranking";
 import MakeMatching from "./pages/Matching/make";
+import PostPage from './pages/Community/post';
 
 export default function App() {
   return (
@@ -19,11 +20,12 @@ export default function App() {
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/user/signup" element={<SignupPage />}></Route>
           <Route path="/user/login" element={<LoginPage />}></Route>
-          <Route path="/notice" element={<CommunityPage />}></Route>
+          <Route path="/board/list" element={<CommunityPage />}></Route>
           <Route path="/match" element={<MatchingPage />}></Route>
-          <Route path="/notice/post" element={<WritePage />}></Route>
+          <Route path="/board" element={<WritePage />}></Route>
           <Route path="/ranking" element={<RankingPage />}/>
           <Route path="/makematching" element={<MakeMatching />}/>
+          <Route path="/board/{id}" element={<PostPage />}></Route>
         </Routes>
       </BrowserRouter>
     </StyleProvider>
